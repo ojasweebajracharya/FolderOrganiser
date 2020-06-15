aceff2be3bd506a11d39f25bd2039b0885f3c8d7
Ojaswee Professional <ojasweebajracharya@gmail.com>
2020-06-15 23:27:43 +0100

    Started working on duplicates

--- venv/organiser.py
+++ venv/organiser.py
@@ -3,9 +3,10 @@ import os
 import shutil
 from PIL import Image
 import re
+from PIL.ExifTags import TAGS
 
+# Retrieves the date from the folder's name - if no date, returns none
 def getDate(folderName):
-    # Using regex - Works wherever the date is in the title:
     regex = r"[2][0][0-2][0-9]"
     matchDate = re.search(regex, str(folderName))
 
@@ -15,6 +16,29 @@ def getDate(folderName):
         return matchDate.group()
 
 
+# Get date from the first images metadata:
+def metadataDate(currentDirectory, folderName):
+    # First go inside folder, look at metadata of picture one
+    with os.scandir(currentDirectory) as entries:
+        for entry in entries:
+
+            break
+    # Retrieve the date and print it out.
+
+
+def isDuplicate(folderName, moveToDirectory, moveToFolder):
+    flag = False
+    moveTo = f"{moveToDirectory}\{moveToFolder}"
+    print(f"moveto: {moveTo}")
+    with os.scandir(moveTo) as entries:
+        for entry in entries:
+            destFolderName = entry.name
+            if folderName == destFolderName:
+                flag = True
+                break
+
+    return flag
+
 def findAndMove(currentDirectory, moveToDirectory, date, folderName):
 
     foundFlag = False
@@ -22,29 +46,42 @@ def findAndMove(currentDirectory, moveToDirectory, date, folderName):
     with os.scandir(moveToDirectory) as entries:
         for entry in entries:
             folderDate = getDate(entry)
+            currentFolder = (f"{currentDirectory}\{folderName}")
+            print(isDuplicate(folderName, moveToDirectory, entry.name))
 
-            if folderDate == date:
-                print(f"We've found a matching folder for {date}!")
-                destinationFolder = (f"{moveToDirectory}\{date}")
-                currentFolder = (f"{currentDirectory}\{folderName}")
-                print(f"Destination Folder: {destinationFolder}")
-                print(f"Current Directory: {currentFolder}")
+            if isDuplicate == False:
 
-                dest = shutil.move(currentFolder, destinationFolder)
-                print("Moved successfully! :D")
-                foundFlag = True
-                break
+                if folderDate == date:
+                    print(f"We've found a matching folder for {date}!")
+
+                    destinationFolder = (f"{moveToDirectory}\{date}")
+
+                    print(f"Destination Folder: {destinationFolder}")
+                    print(f"Current Directory: {currentFolder}")
+
+                    dest = shutil.move(currentFolder, destinationFolder)
+                    print("Moved successfully! :D")
+                    foundFlag = True
+                    break
+
+                if foundFlag == False:
+                    answer = input(f"There are no folders with the year {date}, would you like to make a new folder for it?")
+
+                    if answer == "Y":
+                        # Create a folder with title of the date and run function again
+                        newDir = os.path.join(moveToDirectory, date)
+                        os.mkdir(newDir)
+
+                        findAndMove(currentDirectory, moveToDirectory, date, folderName)
+                    else:
+                        print(f"You've chosen not to create a folder for {date}")
 
 
-        if foundFlag == False:
-            answer = input(f"There are no folders with the date {date}, would you like to make a new folder for it?")
-
-            if answer == "Y":
-                # Create a folder with title of the date and run function again
-
-                findAndMove(currentDirectory, moveToDirectory, date)
             else:
-                print(f"You've chosen not to create a folder for {date}")
+
+                destinationFolder = (f"{moveToDirectory}\Duplicates")
+                dest = shutil.move(currentFolder, destinationFolder)
+                print("This file is a duplicate so I've placed it inside the folder 'duplicates'")
 
 
 
@@ -66,12 +103,13 @@ if isExistCurrent and isExistMoveTo:
             if getDate(entry) != None:
                 entryDate = getDate(entry)
 
+                folderName = entry.name
 
 
                 print(f"entryDate: {entryDate}")
-                print(f"entry.name: {entry.name}")
+                print(f"entry.name: {folderName}")
 
-                findAndMove(currentDirectory, moveToDirectory, entryDate, entry.name)
+                findAndMove(currentDirectory, moveToDirectory, entryDate, folderName)
 
                 # check = input("Would you like to continue? Y or N: ")
                 #
@@ -84,6 +122,10 @@ if isExistCurrent and isExistMoveTo:
                 # else:
                 #     print("You did not enter either 'Y' or 'N' so I will assume you wish not to continue.")
                 #     break
+
+            else:
+                print("The folder does not have a date in the title so I will analyse the metadata.")
+                 # Run metadata function
 
 else:
     print("Your paths do not exist.")
